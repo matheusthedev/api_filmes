3201cc60cdae203dc64d6d6c0dd593066d2faaf2
Matheus Reis <matheus.r.araujo11@aluno.senai.br>
2025-02-20 08:48:57 -0300

    Feat: subindo projeto api filmes

---
+++ Context/Filmes_Context.cs
@@ -0,0 +1,25 @@
+﻿using api_filmes_senai.Domains;
+using Microsoft.EntityFrameworkCore;
+
+namespace api_filmes_senai.Context
+{
+    public class Filmes_Context : DbContext
+    {
+        public Filmes_Context() { }
+
+        public Filmes_Context(DbContextOptions<Filmes_Context> options) : base(options)
+        {
+        }
+
+        public DbSet<Genero> Generos { get; set; }
+        public DbSet<Filme> Filmes { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+                optionsBuilder.UseSqlServer("Server=NOTE36-S28\\SQLEXPRESS; Database=Filmes; User Id=sa; Pwd=Senai@134; TrustServerCertificate=true;");
+            }
+        }
+    }
+}
---
+++ Domains/Filme.cs
@@ -0,0 +1,22 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace api_filmes_senai.Domains
+{
+    public class Filme
+    {
+        [Key]
+        public Guid IdFilme { get; set; }
+
+        [Column(TypeName = "VARCHAR(50)")]
+        [Required(ErrorMessage = "O titulo do filme é obrigatório!")]
+        public String? Titulo { get; set; }
+
+        /// <summary>
+        /// Referência da tabela Genero
+        /// </summary>
+        public Guid IdGenero { get; set; }
+        [ForeignKey("IdGenero")]
+        public Genero? Genero { get; set; }
+    }
+}
